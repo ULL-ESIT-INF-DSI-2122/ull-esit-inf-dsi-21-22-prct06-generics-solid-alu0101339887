059c15134256909496309712c7ce7f2e5412e510
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-03-23 22:20:11 +0000

    Resolviendo el ejercicio 3

--- src/Ejercicio-03/cifrado.ts
+++ src/Ejercicio-03/cifrado.ts
@@ -1,3 +1,38 @@
 export class Cifrado {
-  
+  private cadena: string;
+  private clave: string;
+  private cadenaClave: string;
+  private posLetras: string;
+  private alfabeto: string;
+
+  constructor(cadena: string, clave: string, alfabeto?: string) {
+    this.cadena = cadena;
+    this.cadenaClave = "";
+    this.posLetras = "";
+    this.clave = clave;
+    this.alfabeto = alfabeto || "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  }
+
+  cifrar() {
+
+  }
+
+  rellenarCadenaClave() {
+    while (this.cadenaClave.length < this.cadena.length) {
+      let tam = this.cadena.length - this.cadenaClave.length;
+      if (tam < this.clave.length) 
+        for (let i = 0; i < tam; i++) 
+          this.cadenaClave = this.cadenaClave + this.clave[i];
+      else this.cadenaClave = this.cadenaClave + this.clave;
+    }
+  }
+
+  comprobarPos(letra: string): number | string {
+    let counter = 1;
+    for (let i = 0; i < this.alfabeto.length; i++) {
+      if (letra === this.alfabeto[i]) return counter;
+      counter++;
+    }
+    return `ERROR. La letra ${letra} no pertenece al alfabeto. \n--> Vuelva a ejecutar el cifrado añadiendo otra clave`;
+  }
 }
