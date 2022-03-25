257281936e0c6464adf89aa0d197e0fb557b4def
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-03-25 19:42:41 +0000

    Ejercicio 3-

--- src/Ejercicio-03/Cifrado.ts
+++ src/Ejercicio-03/Cifrado.ts
@@ -2,6 +2,7 @@ export class Cifrado {
   private cadena: string;
   private clave: string;
   private alfabeto: string;
+  private result: string = "";
 
   constructor(cadena: string, clave: string, alfabeto?: string) {
     this.cadena = cadena;
@@ -15,13 +16,31 @@ export class Cifrado {
       if (this.cadena[i] !== " ") str = str + this.cadena[i];
     this.cadena = str;
   }
-
-  cifrar() {
+  
+  cifrado(aux: boolean): void{
+    this.eliminarEspacios();
+    let desplazamiento = 0;
+    let nuevaPos = 0;
     for (let i = 0; i < this.cadena.length; i++) {
       for (let j = 0; j < this.clave.length; j++) {
-        
+        if (this.result.length === this.cadena.length) break;
+        desplazamiento = this.alfabeto.indexOf(this.clave[j]) + 1;
+        if (aux) nuevaPos = (desplazamiento + this.alfabeto.indexOf(this.cadena[i])) % this.alfabeto.length;
+        else nuevaPos = (this.alfabeto.indexOf(this.cadena[i]) - desplazamiento + this.alfabeto.length) % this.alfabeto.length;
+        this.result = this.result + this.alfabeto[nuevaPos];
+        i++;
       }
+      i--;
     } 
   }
 
+  cifrar(): string {
+    this.cifrado(true);
+    return this.result;
+  }
+
+  descifrar(): string {
+    this.cifrado(false);
+    return this.result;
+  }
 }
